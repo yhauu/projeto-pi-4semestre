b7e5b77bafdef9a04e4821f0cb7642e4e64494fc
Fabricio Belomo <fabriciobelomo@gmail.com>
2022-03-02 21:02:30 -0300

    Inicio do projeto

---
+++ projeto-games/src/main/java/com/jogayjoga/projetogames/ProjetoGamesApplication.java
@@ -0,0 +1,13 @@
+package com.jogayjoga.projetogames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGamesApplication.class, args);
+	}
+
+}
---
+++ projeto-games/src/test/java/com/jogayjoga/projetogames/ProjetoGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogayjoga.projetogames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
