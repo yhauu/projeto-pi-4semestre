02053b30adc53b7262b2ea7d64ff0917f10b1798
felype-carvalho <lype.carvalho09@hotmail.com>
2022-03-10 00:19:50 -0300

    Visual tela de usuários cadastrados

--- webapp/js/mainFunction.js
+++ webapp/js/mainFunction.js
@@ -6,3 +6,30 @@ $(function () {
         $(this).load(file)
     })
 })
+
+// Function DataTable
+$(document).ready(function () {
+    $('table').DataTable({
+        dom: 'Bfrtip',
+        // order: [[0,'asc']],
+        order: [],
+        // stateSave: true,
+        colReorder: true,
+        rowReorder: true,
+        responsive: true,
+        // select: true,
+        language: {
+            info: "Mostrando _START_ a _END_ de _TOTAL_ de registros",
+            infoEmpty: " ",
+            emptyTable: "N�o existem registros para exibir",
+            zeroRecords: "Nenhum registro encontrado para pesquisa",
+            processing: "Processando...",
+            paginate: {
+                previous: "Anterior",
+                next: "Pr�ximo"
+            },
+            search: "Pesquisar na tabela",
+
+        }
+    });
+});
