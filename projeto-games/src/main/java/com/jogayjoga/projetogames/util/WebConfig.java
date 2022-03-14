3ba3bd346725463056a4f3560a5ce24f6f3ffa0d
Fabricio Belomo <fabriciobelomo@gmail.com>
2022-03-13 22:22:12 -0300

    Correcao da classe web config para permitir todas as chamadas ao backend

--- projeto-games/src/main/java/com/jogayjoga/projetogames/util/WebConfig.java
+++ projeto-games/src/main/java/com/jogayjoga/projetogames/util/WebConfig.java
@@ -11,9 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/");
+        registry.addMapping("/**");
 
-        registry.addMapping("/")
+        registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
     }
 }
