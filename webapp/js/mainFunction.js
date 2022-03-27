9d9d4f187835748b2fdc3fef6a70cbe9980deef2
Hsilva68 <henrique16868@hotmail.com>
2022-03-27 01:29:47 -0300

    Criada mascara do campo valor

--- webapp/js/mainFunction.js
+++ webapp/js/mainFunction.js
@@ -42,7 +42,8 @@ function startDataTable() {
 $(document).ready(function(){
     $('.date').mask('00/00/0000');
     $('.phone_with_ddd').mask('(00) 0000-0000');
-    $('.cpf').mask('000.000.000-00', {reverse: true});
+    $('.cpf').mask('000.000.000-00', {reverse: true})
+    $('.value').mask('R$ 000,00');
   });
 
 // Format Date
