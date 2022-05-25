accd46b5a370cc778b2f5ae60edda9c9a698b40b
felype-carvalho <lype.carvalho09@hotmail.com>
2022-05-25 02:18:25 -0300

    Tratamento para checkout

--- webapp/js/service/checkoutService.js
+++ webapp/js/service/checkoutService.js
@@ -1,24 +1,112 @@
-function checkout() {
-    let pagamentoStatus = validaPagamento()
+function checkout(metodoPagamento) {
 
-    if (pagamentoStatus === "boleto" || pagamentoStatus === "cartao") {
+    let frete = parseFloat(localStorage.getItem("frete"))
+    let valTotal = calcSubtotalCarrinho() + frete
+    let listaProdutos = JSON.parse(localStorage.getItem("carrinho") || "[]")
+    let idCliente = parseInt(localStorage.getItem("idClient"))
+    let endEntrega = parseInt(localStorage.getItem("endEntrega"))
+    let endFatura = parseInt(localStorage.getItem("endFatura"))
+
+
+
+    if (metodoPagamento === "BOLETO" || metodoPagamento === "CARTAO_CREDITO") {
         console.log("Foi")
-        
+        let data = {
+            fee: frete,
+            totalSaleAmount: valTotal,
+            clientId: idCliente,
+            deliveryAddressId: endEntrega,
+            billingAdrressId: endFatura,
+            paymentMethods: metodoPagamento
+        }
+        console.log(data)
+
+        let success = function (data) {
+            alert("Pedido Realizado com Sucesso!")
+            window.location = "store-list-order.html"
+        }
+
+        let error = function (err) {
+            console.log(err)
+        }
+
     }
 }
 
-function validaPagamento(){
+function validaPagamento() {
     let inputBoleto = document.getElementById("payment-1")
     let inputCartao = document.getElementById("payment-2")
 
     if (inputBoleto.checked === true) {
-        return inputBoleto.value
+        checkout(inputBoleto.value)
     } else if (inputCartao.checked === true) {
-        return inputCartao.value
+        checkout(inputCartao.value)
     } else {
         alert("Selecione o método de pagamento!!")
-        return null
+        checkout(null)
+    }
+}
+
+function listClientAddressCheckout() {
+    let idCliente = parseInt(localStorage.getItem("idClient"))
+
+    let success = function (data) {
+        console.log(data)
+
+        document.getElementById("lista-entrega").innerHTML += `
+                            <div class="row">
+                                <div class="col-xs-10">
+                                    <h4 class="product-name"><strong>${data.address[0].address}, ${data.address[0].numberAddress} -
+                                    ${data.address[0].zipCode}</strong></h4>
+                                    <h4><small>${data.address[0].district}, ${data.address[0].city} - SP</small></h4>
+                                </div>
+                                <div class="col-xs-2">
+                                    <div class="btn-group" data-toggle="buttons">
+                                        <label class="btn btn-danger">
+                                            <input type="radio" name="options" id="option2" autocomplete="off">
+                                            <i class="fa fa-check"></i>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>`
+
+        document.getElementById("lista-faturamento").innerHTML += `
+                            <div class="row">
+                                <div class="col-xs-10">
+                                    <h4 class="product-name"><strong>${data.address[1].address}, ${data.address[1].numberAddress} -
+                                    ${data.address[1].zipCode}</strong></h4>
+                                    <h4><small>${data.address[1].district}, ${data.address[1].city} - SP</small></h4>
+                                </div>
+                                <div class="col-xs-2">
+                                    <div class="btn-group" data-toggle="buttons">
+                                        <label class="btn btn-danger">
+                                            <input type="radio" name="options" id="option2" autocomplete="off">
+                                            <i class="fa fa-check"></i>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>`
+
+        localStorage.setItem("endEntrega", data.address[0].id)
+        localStorage.setItem("endFatura", data.address[1].id)
     }
 
-   
+    let error = function (err) {
+        console.log(err)
+    }
+
+    findOneClient(success, error, idCliente)
+
+}
+
+function findOneClient(success, error, id) {
+    $.ajax({
+        url: urlPrincipal + urlClient + `/${id}`,
+        contentType: 'application/json',
+        type: 'GET',
+        success,
+        error,
+    })
 }
